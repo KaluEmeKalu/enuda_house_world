b9624101645699cf5ecbfcfcb1173fec099e1d4e
Kalu Kalu <1718221904@qq.com>
2018-02-01 00:11:49 +0800

    added init migration and settings

--- enuda_house_world/settings.py
+++ enuda_house_world/settings.py
@@ -38,6 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'enuda_mag',
+    'tinymce',
+    'imagekit',
+    'widget_tweaks',
+    'tracking',
 ]
 
 MIDDLEWARE = [
